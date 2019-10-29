e2b282ca8e4cc3991aebc650b8f7df74d6fbad67
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-10-29 18:02:53 -0400

    started my javascript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,3 +7,27 @@ var connection = mysql.createConnection({
     password:"3317",
     database: "bamazon"
 });
+
+connection.connect();
+
+begin ()
+
+function begin () {
+connection.query("SELECT * FROM products", function (error, res) {
+    if(error) throw error;
+    res.forEach(row => {
+        console.log("Id: ${row.item_id} Name: ${row.product_name} Price: ${row.price}\n"))
+    });
+
+    myquestions()
+
+    function myquestions() {
+        inquirer.prompt([
+            {
+                
+            }
+        ])
+    }
+})
+}
+
