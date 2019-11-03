2f0428edda970066ab536b97b66146c1bd8431b5
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-11-03 18:55:32 -0500

    added comments and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Bamazon-app
+
+Hi this is my Bamazon app. It's supposed to be like Amazon, except it isn't. Customers are presented with a list of items to choose from, they enter the item they want and how many units and the mySQL database will give them the items according to how many are in stock. The user is then either given the total price of the transaction or they are told that the item is no longer in stock.
+
+In order to use Bamazon:
+
+1. Have a mySQL database set up on your computer. Then include the following:
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+    You will have to create your own password.
+
+2. Git clone my repo: https://github.com/davidmstanleyjr/Bamazon-app.git
+3. From git bash do npm install 
+Inquirer: https://www.npmjs.com/package/inquirer
+MySQL: https://www.npmjs.com/package/mysql
+
+4. From git bash run node bamazonCustomer.js. There will be a list of items to choose from. Type in your item and you can either purchase it or you will be told that it's no longer in stock.
+
+I hope you enjoy it. I had a great time creating it.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
+//inquirer and mysql variables
+
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+//connects to mySQL database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,7 +13,7 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
-
+//this is the logic for completing an order. 
 function myOrder(productObject, productName, productQuantity) {
     var newQuantity = productObject.stock_quantity - productQuantity;
     var sold = productObject.price * productQuantity;
@@ -23,7 +26,7 @@ function myOrder(productObject, productName, productQuantity) {
     });
 };
 
-
+//this updates the stock of items in my mysql database.
 function removeProduct(productName, productQuantity) {
     connection.query("SELECT * FROM products", function (error, res) {
         if (error) throw error;
@@ -33,11 +36,12 @@ function removeProduct(productName, productQuantity) {
                 product = res[i]
             }
         }
-
+        //this is the logic for finding the product in the database, displaying the total costs of the purchase 
+        //and telling the user that there is insufficient quantity if they order more than what we have in stock.
         console.log(product, "We found the product")
         if (product.stock_quantity >= productQuantity) {
             myOrder(product, productName, productQuantity)
-            console.log("Total costs = $",productQuantity * product.price)
+            console.log("Total costs = $", productQuantity * product.price)
         }
         else {
             console.log("insufficient quantity!")
@@ -47,6 +51,8 @@ function removeProduct(productName, productQuantity) {
     })
 };
 
+
+//this is the logic that asks the customer what they want how many they would like.
 function myquestions() {
     inquirer.prompt([
         {
@@ -65,26 +71,24 @@ function myquestions() {
         var productId = answer.productId;
         var productQuantity = answer.productQuantity;
         removeProduct(productId, productQuantity)
-    }).catch(function(error){console.log(error)});
+    }).catch(function (error) { console.log(error) });
 }
 
 begin();
-
+//this starts the entire transaction. I originally had all other functions inside of this function. The app wouldn't work as
+//a result. I took the other functions out and left this at the end.
 function begin() {
     connection.query("SELECT * FROM products", function (error, res) {
         if (error) throw error;
         for (let i = 0; i < res.length; i++) {
             console.log("Id: " + res[i].item_id + "Product: " + res[i].product_name + "Price: " + res[i].price + "This is what we have left for you to purchase: " + res[i].stock_quantity);
-        }});
+        }
+    });
 
     myquestions();
 
-    
 
-    
 
-    
 
-   
 
 }
